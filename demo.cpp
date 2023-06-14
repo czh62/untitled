8ece6b2ccbaba620d5c4d18427ba2b82bb7cf74f
Zehao Chen <mikechenzehao@126.com>
2023-06-14 11:33:56 +0800

    demo

---
+++ demo.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+// 求(x*y)%n
+int64_t mod_mux(int64_t x, int64_t y, int64_t n)
+{
+    int64_t ret = 0, tmp = x % n;
+    while (y)
+    {
+        if (y & 0x1)
+            if ((ret += tmp) > n)
+                ret -= n;
+        if ((tmp <<= 1) > n)
+            tmp -= n;
+        y >>= 1;
+    }
+    return ret;
+}
+
+// 模重复平方算法求(a^b) % c
+int64_t mod_pow(int64_t a, uint64_t b, int64_t c)
+{
+    int64_t ret = 1;
+    while (b)
+    {
+        if (b & 0x1)
+            ret = mod_mux(ret, a, c);
+        a = mod_mux(a, a, c);
+        b >>= 1;
+    }
+    return ret;
+}
+
+bool IsPrime(int64_t n)
+{
+    // millerTest
+    int64_t t = 100; // 100次检验
+    int64_t k = 0, m, a, i;
+
+    random_device rd;
+    mt19937_64 gen(rd());
+    uniform_int_distribution<int64_t> dis(2, n - 2);
+
+    for (m = n - 1; !(m & 1); m >>= 1, k++)
+        ;
+    while (t--)
+    {
+        a = mod_pow(dis(gen), m, n);
+        if (a != 1)
+        {
+            for (i = 0; i < k && a != n - 1; i++)
+                a = mod_mux(a, a, n);
+            if (i >= k)
+                return false;
+        }
+    }
+    return true;
+}
+
+int64_t ProduceRandomPrime()
+{
+    while (true)
+    {
+        // 生成
+        random_device rd;
+        mt19937_64 gen(rd());
+        uniform_int_distribution<int64_t> dis(0x0000800000000000, 0x0000ffffffffffff);
+
+        int64_t n = dis(gen);
+        // 奇数
+        n |= 1;
+        // 判断
+        if (IsPrime(n) && IsPrime(2 * n + 1))
+            return 2 * n + 1;
+        cout << 2 * n + 1 << "false" << endl;
+    }
+}
+
+int64_t PrimitiveElement(int64_t n)
+{
+    int64_t p = (n - 1) / 2;
+    while (true)
+    {
+        random_device rd;
+        mt19937_64 gen(rd());
+        uniform_int_distribution<int64_t> dis(2, n - 2);
+        int64_t g;
+        do
+        {
+            g = dis(gen);
+            cout << g << "try" << endl;
+        } while (mod_pow(g, 2, n) == 1 || mod_pow(g, p, n) == 1);
+        return g;
+    }
+}
+
+
+int main() {
+    random_device rd;
+    mt19937_64 gen(rd());
+    uniform_int_distribution<int64_t> dis(0x0000800000000000, 0x0000ffffffffffff);
+    int64_t a = dis(gen);
+    int64_t b = dis(gen);
+    int64_t c = dis(gen);
+    int64_t n = ProduceRandomPrime();
+    int64_t g = PrimitiveElement(n);
+    cout << "大素数：\t" << n << endl;
+    cout << "原  根：\t" << g << endl;
+    cout << "随机数a：\t" << a << endl;
+    cout << "随机数b：\t" << b << endl;
+    cout << "随机数c：\t" << c << endl;
+    cout << "---------计算---------"<< endl;
+    int64_t Sa = mod_pow(g,a,n);
+    cout << "Sa = g^a(modn) =     " << Sa << endl;
+    int64_t Sab = mod_pow(Sa,b,n);
+    cout << "Sab = Sa^b(modn) =   " << Sab << endl;
+    int64_t Sabc = mod_pow(Sab,c,n);
+    cout << "Sabc = Sab^c(modn) = " << Sabc << "(协商密钥)";
+
+}
